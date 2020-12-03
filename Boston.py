8cd610a4604d135e7b118262a2a7db7ffd406ba6
shadab-stack <66989936+shadab-stack@users.noreply.github.com>
2020-12-03 11:59:31 +0530

    Add files via upload

---
+++ Boston.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import ShuffleSplit
+
+# Import supplementary visualizations code visuals.py
+import visuals as vs
+
+# Pretty display for notebooks
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+# Load the Boston housing dataset
+data = pd.read_csv('housing.csv')
+prices = data['MEDV']
+features = data.drop('MEDV', axis = 1)
+    
+# Success
+print("Boston housing dataset has {} data points with {} variables each.".format(*data.shape))
+
+
+# In[5]:
+
+
+np.amin(prices)
+
+
+# In[6]:
+
+
+# Minimum price of the data
+minimum_price = np.amin(prices)
+
+# Maximum price of the data
+maximum_price = np.amax(prices)
+
+# Mean price of the data
+mean_price = np.mean(prices)
+
+# Median price of the data
+median_price = np.median(prices)
+
+# Standard deviation of prices of the data
+std_price = np.std(prices)
+
+# Show the calculated statistics
+print("Statistics for Boston housing dataset:\n")
+print("Minimum price: ${}".format(minimum_price)) 
+print("Maximum price: ${}".format(maximum_price))
+print("Mean price: ${}".format(mean_price))
+print("Median price ${}".format(median_price))
+print("Standard deviation of prices: ${}".format(std_price))
+
+
+# In[7]:
+
+
+# Import 'r2_score'
+
+from sklearn.metrics import r2_score
+
+def performance_metric(y_true, y_predict):
+    """ Calculates and returns the performance score between 
+        true and predicted values based on the metric chosen. """
+    
+    # TODO: Calculate the performance score between 'y_true' and 'y_predict'
+    score = r2_score(y_true, y_predict)
+    
+    # Return the score
+    return score
+
+
+# In[8]:
+
+
+# Import 'train_test_split'
+from sklearn.model_selection import train_test_split
+
+# Shuffle and split the data into training and testing subsets
+X_train, X_test, y_train, y_test = train_test_split(features, prices, test_size=0.2, random_state = 42)
+
+# Success
+print("Training and testing split was successful.")
+
+
+# In[9]:
+
+
+vs.ModelLearning(features, prices)
+
+
+# In[10]:
+
+
+vs.ModelComplexity(X_train, y_train)
+
+
+# In[11]:
+
+
+# Import 'make_scorer', 'DecisionTreeRegressor', and 'GridSearchCV'
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import make_scorer
+from sklearn.model_selection import GridSearchCV
+
+def fit_model(X, y):
+    """ Performs grid search over the 'max_depth' parameter for a 
+        decision tree regressor trained on the input data [X, y]. """
+    
+    # Create cross-validation sets from the training data
+    cv_sets = ShuffleSplit(n_splits = 10, test_size = 0.20, random_state = 0)
+
+    # Create a decision tree regressor object
+    regressor = DecisionTreeRegressor()
+
+    # Create a dictionary for the parameter 'max_depth' with a range from 1 to 10
+    params = {'max_depth':[1,2,3,4,5,6,7,8,9,10]}
+
+    # Transform 'performance_metric' into a scoring function using 'make_scorer' 
+    scoring_fnc = make_scorer(performance_metric)
+
+    # Create the grid search cv object --> GridSearchCV()
+    # Make sure to include the right parameters in the object:
+    # (estimator, param_grid, scoring, cv) which have values 'regressor', 'params', 'scoring_fnc', and 'cv_sets' respectively.
+    grid = GridSearchCV(estimator=regressor, param_grid=params, scoring=scoring_fnc, cv=cv_sets)
+
+    # Fit the grid search object to the data to compute the optimal model
+    grid = grid.fit(X, y)
+
+    # Return the optimal model after fitting the data
+    return grid.best_estimator_
+
+
+# In[12]:
+
+
+
+# Fit the training data to the model using grid search
+reg = fit_model(X_train, y_train)
+
+# Produce the value for 'max_depth'
+print("Parameter 'max_depth' is {} for the optimal model.".format(reg.get_params()['max_depth']))
+
+
+# In[13]:
+
+
+
+# Produce a matrix for client data
+client_data = [[5, 17, 15], # Client 1
+               [4, 32, 22], # Client 2
+               [8, 3, 12]]  # Client 3
+
+# Show predictions
+for i, price in enumerate(reg.predict(client_data)):
+    print("Predicted selling price for Client {}'s home: ${:,.2f}".format(i+1, price))
+
+
+# In[14]:
+
+
+
+vs.PredictTrials(features, prices, fit_model, client_data)
+
+
+# In[ ]:
+
+
+
+
